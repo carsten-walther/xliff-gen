dfb7138a01ce9c84e181b11c782fc12c550af261
Carsten Walther <walther.carsten@web.de>
2021-01-24 13:53:37 +0100

    Add error logging

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 
+$basePath = __DIR__;
+
 // for infinite time of execution
 ini_set('max_execution_time', '0');
+
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 'On');
+ini_set('error_log', $basePath . '/php_error.log');
 
 /**
  * run: php -S localhost:8000
  */
-
-$basePath = __DIR__;
 
 require $basePath . '/vendor/autoload.php';
 
