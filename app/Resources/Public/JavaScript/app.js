9e2e6a6513dc51488e89061b363d5b42211d9a2d
Carsten Walther <walther.carsten@web.de>
2021-01-24 13:57:21 +0100

    Improve select on resize

--- app/Resources/Public/JavaScript/app.js
+++ app/Resources/Public/JavaScript/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function($) {
-
   $('select').select2({
     placeholder: 'Select an option'
   });
+});
 
+$(window).resize(function () {
+  $('select').select2({
+    placeholder: 'Select an option'
+  });
 });
